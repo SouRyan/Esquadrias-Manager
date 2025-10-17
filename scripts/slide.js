47accccb0b20e9fb560cef8684a04a4e172bc93f
SouRyan <ryanbrayan.fs@gmail.com>
2025-10-16 21:51:48 -0300

    first commit

---
+++ scripts/slide.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', function() {
+  
+//     new Splide('#heritage-carousel', {
+//         type: 'loop',
+//         perPage: 3,
+//         perMove: 3,
+//         gap: '20px',
+//         autoplay: true,
+//         interval: 4000,
+//         pauseOnHover: true,
+//         pagination: true,
+//         arrows: true,
+//         breakpoints: {
+//             768: {
+//                 arrows: false
+//             }
+//         }
+//     }).mount();
+//     const splideList = document.querySelector(".splide__list");
+//     if (splideList) {
+//       splideList.style.transform = "translateX(0px)";
+//     }
+// });
